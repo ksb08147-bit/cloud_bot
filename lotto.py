83fbbbd79099035542723f61a5c42372e8bfbbc2
ksb08147-bit <ksb08147@gmail.com>
2026-06-17 00:34:06 +0900

    Update lotto.py

--- lotto.py
+++ lotto.py
@@ -2,4 +2,4 @@ import random
 
 def make_lotto():
     numbers = sorted(random.sample(range(1, 46), 6))
-    return "🎟️ 이번 로또 번호는 " + ", ".join(map(str, numbers))
+    return "이번에는..." + ",".join(map(str, numbers)) + "번으로..어때?"
